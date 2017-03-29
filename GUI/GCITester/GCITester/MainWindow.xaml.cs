fc5428adbfa4332255a3efd7d5bf56c11ee7f74f
oparra22 <oparra1995@gmail.com>
2017-03-29 16:54:07 -0600

    Adding subfolders

---
+++ GUI/GCITester/GCITester/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GCITester
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+
+            InitializeComponent();
+
+            
+        }
+
+        private void openPort_Click(object sender, RoutedEventArgs e)
+        {
+            testsButton.IsEnabled = true; 
+        }
+
+        private void settingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            settings window = new settings();
+            window.Show();
+            //this.Close();
+        }
+
+        private void testsButton_Click(object sender, RoutedEventArgs e)
+        {
+            tests window = new tests();
+            window.Show();
+            this.Close();
+        }
+
+        private void reportsButton_Click(object sender, RoutedEventArgs e)
+        {
+            reports window = new reports();
+            window.Show();
+            this.Close();
+        }
+
+        private void aboutButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ GUI/GCITester/GCITester/pinTest.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GCITester
+{
+    /// <summary>
+    /// Interaction logic for pinTest.xaml
+    /// </summary>
+    public partial class pinTest : Window
+    {
+
+        public pinTest()
+        {
+            InitializeComponent();
+        }
+
+
+        private void testButton_Click(object sender, RoutedEventArgs e)
+        {
+            listBox.Items.Add("Pin " + "Measured");
+           // listBox.SelectedIndex = listBox.Items.Count - 1;
+        }
+     
+
+       
+    }
+}
