8c2be76f5182dcea6f776b62ec8f7ad8b3874e56
sethpearce85 <sethpearce85@gmail.com>
2017-04-12 19:11:24 -0600

    Revert "Revert "Added SQL scripts for setup and dummy data""
    This reverts commit 2de1a75588f361a2c1a396fed1fb0da9578ae0ff.

---
+++ DB/Dump/DB Dummy Data/gci_routines.sql
@@ -0,0 +1,155 @@
+CREATE DATABASE  IF NOT EXISTS `gci` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `gci`;
+-- MySQL dump 10.13  Distrib 5.5.16, for Win32 (x86)
+--
+-- Host: localhost    Database: gci
+-- ------------------------------------------------------
+-- Server version	5.5.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Temporary table structure for view `productiondata_v`
+--
+
+DROP TABLE IF EXISTS `productiondata_v`;
+/*!50001 DROP VIEW IF EXISTS `productiondata_v`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE TABLE `productiondata_v` (
+  `ProductionTestID` int(11),
+  `CreationDate` datetime,
+  `BatchName` varchar(45),
+  `PartName` varchar(45),
+  `DutPinNumber` int(11),
+  `BaselineVoltage` double,
+  `AverageVoltage` double,
+  `StdDevVoltage` double,
+  `TestResult` bit(1)
+) ENGINE=MyISAM */;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Temporary table structure for view `lifetimedata_v_individuals`
+--
+
+DROP TABLE IF EXISTS `lifetimedata_v_individuals`;
+/*!50001 DROP VIEW IF EXISTS `lifetimedata_v_individuals`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE TABLE `lifetimedata_v_individuals` (
+  `LifetimeTestID` int(11),
+  `SerialNumber` varchar(50),
+  `BatchName` varchar(50),
+  `PartName` varchar(45),
+  `TestHour` int(11),
+  `LowerRange` double,
+  `UpperRange` double,
+  `Temperature` double,
+  `DUTPinNumber` int(11),
+  `MeasurementNumber` int(11),
+  `MeasuredVoltage` double,
+  `CreationDate` datetime
+) ENGINE=MyISAM */;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Temporary table structure for view `lifetimedata_v_average`
+--
+
+DROP TABLE IF EXISTS `lifetimedata_v_average`;
+/*!50001 DROP VIEW IF EXISTS `lifetimedata_v_average`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE TABLE `lifetimedata_v_average` (
+  `LifetimeTestID` int(11),
+  `SerialNumber` varchar(50),
+  `BatchName` varchar(50),
+  `PartName` varchar(45),
+  `TestHour` int(11),
+  `LowerRange` double,
+  `UpperRange` double,
+  `Temperature` double,
+  `DUTPinNumber` int(11),
+  `AverageVoltage` double,
+  `StdDevVoltage` double,
+  `CreationDate` datetime
+) ENGINE=MyISAM */;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Final view structure for view `productiondata_v`
+--
+
+/*!50001 DROP TABLE IF EXISTS `productiondata_v`*/;
+/*!50001 DROP VIEW IF EXISTS `productiondata_v`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8 */;
+/*!50001 SET character_set_results     = utf8 */;
+/*!50001 SET collation_connection      = utf8_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `productiondata_v` AS select `pd`.`ProductionTestID` AS `ProductionTestID`,`pd`.`CreationDate` AS `CreationDate`,`pd`.`BatchName` AS `BatchName`,`p`.`PartName` AS `PartName`,`pd`.`DUTPinNumber` AS `DutPinNumber`,`pl`.`AverageVoltage` AS `BaselineVoltage`,`pd`.`AverageVoltage` AS `AverageVoltage`,`pd`.`StdDevVoltage` AS `StdDevVoltage`,`pd`.`TestResult` AS `TestResult` from ((`productiondata` `pd` left join `part` `p` on((`pd`.`PartID` = `p`.`PartID`))) left join `productionlimits` `pl` on(((`pd`.`ProductionLimitID` = `pl`.`ProductionLimitID`) and (`pd`.`DUTPinNumber` = `pl`.`DUTPinNumber`)))) where ((`p`.`IsActive` = 1) and (`pd`.`MeasurementNumber` = 0)) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+
+--
+-- Final view structure for view `lifetimedata_v_individuals`
+--
+
+/*!50001 DROP TABLE IF EXISTS `lifetimedata_v_individuals`*/;
+/*!50001 DROP VIEW IF EXISTS `lifetimedata_v_individuals`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8 */;
+/*!50001 SET character_set_results     = utf8 */;
+/*!50001 SET collation_connection      = utf8_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `lifetimedata_v_individuals` AS select `ld`.`LifetimeTestID` AS `LifetimeTestID`,`ld`.`SerialNumber` AS `SerialNumber`,`ld`.`BatchName` AS `BatchName`,`p`.`PartName` AS `PartName`,`ld`.`TestHour` AS `TestHour`,`ll`.`LowerRange` AS `LowerRange`,`ll`.`UpperRange` AS `UpperRange`,`ld`.`Temperature` AS `Temperature`,`ld`.`DUTPinNumber` AS `DUTPinNumber`,`ld`.`MeasurementNumber` AS `MeasurementNumber`,`ld`.`MeasuredVoltage` AS `MeasuredVoltage`,`ld`.`CreationDate` AS `CreationDate` from ((`lifetimedata` `ld` left join `part` `p` on((`ld`.`PartID` = `p`.`PartID`))) left join `lifetimelimit` `ll` on((`ld`.`LifetimeLimitID` = `ll`.`LifetimeLimitID`))) where (`p`.`IsActive` = 1) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+
+--
+-- Final view structure for view `lifetimedata_v_average`
+--
+
+/*!50001 DROP TABLE IF EXISTS `lifetimedata_v_average`*/;
+/*!50001 DROP VIEW IF EXISTS `lifetimedata_v_average`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8 */;
+/*!50001 SET character_set_results     = utf8 */;
+/*!50001 SET collation_connection      = utf8_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `lifetimedata_v_average` AS select `ld`.`LifetimeTestID` AS `LifetimeTestID`,`ld`.`SerialNumber` AS `SerialNumber`,`ld`.`BatchName` AS `BatchName`,`p`.`PartName` AS `PartName`,`ld`.`TestHour` AS `TestHour`,`ll`.`LowerRange` AS `LowerRange`,`ll`.`UpperRange` AS `UpperRange`,`ld`.`Temperature` AS `Temperature`,`ld`.`DUTPinNumber` AS `DUTPinNumber`,`ld`.`AverageVoltage` AS `AverageVoltage`,`ld`.`StdDevVoltage` AS `StdDevVoltage`,`ld`.`CreationDate` AS `CreationDate` from ((`lifetimedata` `ld` left join `part` `p` on((`ld`.`PartID` = `p`.`PartID`))) left join `lifetimelimit` `ll` on((`ld`.`LifetimeLimitID` = `ll`.`LifetimeLimitID`))) where ((`ld`.`MeasurementNumber` = 0) and (`p`.`IsActive` = 1)) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2012-12-07 12:46:25
