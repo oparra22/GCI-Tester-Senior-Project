e33810c6a7418c57a35d87ace24bb2ff0149856e
kfarhat1 <karlfarhat93@gmail.com>
2017-05-04 14:12:47 -0600

    sethy is a big whore new short test

--- GUI/GCITester/GCITester/shortTest.xaml.cs
+++ GUI/GCITester/GCITester/shortTest.xaml.cs
@@ -27,11 +27,8 @@ namespace GCITester
 
         //public List<int> listOfPins { get; set; }
         public List<int> listPins = new List<int>();
-        private int FuckSeth = 0;
         private int resultsIndex = 0;
         int flag;
-        // public ObservableCollection<int> names = new ObservableCollection<int>();
-
 
 
 
@@ -39,6 +36,7 @@ namespace GCITester
         {
 
             InitializeComponent();
+            iterCount2.setValue(1);
             // List<int> listPins = new List<int>();
             //   PossiblePins.ItemsSource = listPins;
             //  ICollectionView view = CollectionViewSource.GetDefaultView(PossiblePins.ItemsSource);
@@ -50,56 +48,31 @@ namespace GCITester
         //when test pins is clicked
         private void testButton_Click(object sender, RoutedEventArgs e)
         {
+            //get number of iterations
+            int iter = iterCount2.pinValue();
             //int PinIn = Convert.ToInt32(pinIn.pinValue());
             int pin1 = Convert.ToInt32(shortTestControl.pinValue());
             int pin2 = Convert.ToInt32(shortTestControl.PossiblePins.SelectedItem);
             Console.WriteLine("Second pin selected from dropdown menu = " + pin2);
-            Byte Pin1_1;
-            Byte Pin1_2;
-            Byte Pin2_1;
-            Byte Pin2_2;
 
-            if (pin1 > 254)
+            //run the tests based on number of iterations
+            for (int i = 0; i < iter; i++)
             {
-                Pin1_1 = (Byte)254;
-                Pin1_2 = (Byte)(pin1 - 254);
+                flag = 0;
+                Communication.TestPinShort(pin1, pin2);
+                while (true)
+                {
+                    if (flag == 1)
+                    {
+                        break;
+                    }
+                }
             }
-            else
-            {
-                Pin1_1 = (Byte)pin1;
-                Pin1_2 = (Byte)0;
-            }
-            if (pin2 > 254)
-            {
-                Pin2_1 = (Byte)254;
-                Pin2_2 = (Byte)(pin2 - 254);
-            }
-            else
-            {
-                Pin2_1 = (Byte)pin2;
-                Pin2_2 = (Byte)0;
-            }
-            //Communication.TestPinShort(Pin1_1, Pin1_2, Pin2_1, Pin2_2);
-        }
-
-        private void PossiblePins_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            //List<string> data = new List<string>();   
-            var comboBox = sender as ComboBox;
-        }
-
-
-        private void PossiblePins_Loaded(object sender, RoutedEventArgs e)
-        {
 
 
         }
 
-        private void updateButton_Click(object sender, RoutedEventArgs e)
-        {
-            //  PossiblePins_Loaded(sender, e);
-            //  CollectionViewSource.GetDefaultView(PossiblePins.ItemsSource).Refresh();
-        }
+
 
         private void pinIn_Loaded_1(object sender, RoutedEventArgs e)
         {
@@ -108,7 +81,7 @@ namespace GCITester
             // CollectionViewSource.GetDefaultView(PossiblePins.ItemsSource).Refresh();
             //PossiblePins.Items.Clear();
             //PossiblePins_Loaded(sender, e);
-            //Console.WriteLine("Fuck Seth the whore");
+            Console.WriteLine("Fuck Seth the whore");
         }
 
         private void form1_load(object sender, RoutedEventArgs e)
@@ -127,23 +100,23 @@ namespace GCITester
             Console.WriteLine("Onresultecomplete before AddLog");
             //values need to be changed for final PCB
             Double Voltagedrop = (VoltageRef - VoltageIn);
-            Double Current = Voltagedrop / 4700; 
+            Double Current = Voltagedrop / 4700;
             Double Resistance = VoltageIn / Current;
             bool shortPass = false;
             if (Resistance > 1000)
             {
                 shortPass = true;
-                AddLog("Passed. Pins Tested: Pin1:" + TestedPin + " Pin2:" + TestedPin2 + ". Resistance:"  + Resistance + "Ohms." + ". VoltageIn read:" + VoltageIn + " VoltageDrop:" + Voltagedrop);
+                AddLog("Passed. Pins Tested: " + TestedPin + " and " + TestedPin2 + ". Resistance:" + string.Format("{0:0.00}", Resistance) + "Ohms. VoltageDrop:" + Voltagedrop);
 
             }
             else
             {
                 shortPass = false;
-                AddLog("Failed. Pins Tested: Pin1:" + TestedPin + " Pin2:" + TestedPin2 + ". Resistance:" + Resistance + "Ohms." + ". VoltageIn read:" + VoltageIn + " VoltageDrop:" + Voltagedrop);
+                AddLog("Failed. Pins Tested: Pin1: " + TestedPin + " Pin2: " + TestedPin2 + ". Resistance:" + Resistance + "Ohms. VoltageDrop:" + Voltagedrop);
             }
             Communication.SetResultLED(shortPass);
             //AddLog("Pin " + TestedPin.ToString() + " Measured: " + VoltageIn.ToString() + "V  [0x" + Communication.PinValue.ToString("X4") + "]" + "\t Voltage Drop: " + (double)(VoltageRef - Voltage) + "Testedpin 2 = " + TestedPin2);
-            AddLog("Pin1:" + TestedPin +" Pin2:" + TestedPin2 + ". VoltageIn read:" + VoltageIn + " VoltageDrop:" +Voltagedrop+ ". Resistance:" + Resistance+ "Ohms.");
+            //AddLog("Pin1:" + TestedPin +" Pin2:" + TestedPin2 + ". VoltageIn read:" + VoltageIn + " VoltageDrop:" +Voltagedrop+ ". Resistance:" + Resistance+ "Ohms.");
             //AddLog("Bool shortPass = " + shortPass);
             //manualShortTestResults.ScrollIntoView(manualShortTestResults.Items[manualShortTestResults.Items.Count - 1]);
             //manualShortTestResults.ScrollIntoView(manualShortTestResults.SelectedItem);
@@ -172,7 +145,7 @@ namespace GCITester
                 manualShortTestResults.ScrollIntoView(manualShortTestResults.Items[manualShortTestResults.Items.Count - 1]);
             }));
 
-            
+
         }
 
         private void OnWindowClosing(object sender, CancelEventArgs e)
@@ -180,7 +153,7 @@ namespace GCITester
             Communication.OnResultComplete -= new Communication.ResultComplete(Communication_OnResultComplete);
         }
 
-        
+
 
         private void manualShortTestResults_Loaded(object sender, RoutedEventArgs e)
         {
@@ -199,6 +172,77 @@ namespace GCITester
         private void shortTestControl_Loaded(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void testAllButton_Click(object sender, RoutedEventArgs e)
+        {
+            //get number of iterations
+            int iter = iterCount2.pinValue();
+            //get list of possible pins
+            List<int> listPins = new List<int>();
+            //get pin 
+            for (int pin = 1; pin < 10; pin++)
+            {
+                listPins = GetPossibleValues(pin);
+                // Console.WriteLine("Test");
+
+                for (int s = 0; s < listPins.Count(); s++)
+                {
+
+                    Communication.TestPinShort(pin, listPins[s]);
+                    Console.WriteLine("Fucks that shit");
+                    Console.WriteLine(listPins[s]);
+
+                }
+
+            }
+
+            //List<int> PinsToTest = GCIToDUTMap.Keys.ToList<int>();
+        }
+
+        private List<int> GetPossibleValues(int PinIn)
+        {
+            List<int> returnList = new List<int>();
+            int even1;
+            int even2;
+            int pair = (PinIn - 1) / 32;
+            //check if odd or even. and return possible pins to test
+            int odd = PinIn % 2;
+            int channelPins = 32 * pair + 1;
+            Console.WriteLine(odd);
+            if (odd == 1)
+            {
+                //Console.WriteLine("Fuck Baldy");
+                for (int i = channelPins; i < channelPins + 32; i++)
+                {
+                    even1 = i % 2;
+                    //if pin is odd, add to list of possible pins
+                    if (even1 == 0)
+                    {
+                        returnList.Add(i);
+
+                        // Console.WriteLine("Fuck seth");
+                        //PossiblePins.Items.Add(i);
+                    }
+                }
+            }
+            else if (odd == 0)
+            {
+                for (int i = channelPins; i < channelPins + 32; i++)
+                {
+                    even2 = i % 2;
+                    //if pin is even, add to list possible pins
+                    if (even2 == 1)
+                    {
+
+                        returnList.Add(i);
+                        //Console.WriteLine("Fuck Bob");
+                        //PossiblePins.Items.Add(i);
+
+                    }
+                }
+            }
+            return returnList;
         }
     }
 
